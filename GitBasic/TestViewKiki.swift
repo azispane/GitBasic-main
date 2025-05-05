9f0bce2ea28f4e5ef3259945681fe1e54f5a92ba
Muhammad Rifki Hidayatullah <108870501+Dylm54@users.noreply.github.com>
2025-05-05 17:23:37 +0700

    Add new file

---
+++ GitBasic/TestViewKiki.swift
@@ -0,0 +1,18 @@
+//
+//  TestViewKiki.swift
+//  GitBasic
+//
+//  Created by Rifki Hidayatullah on 05/05/25.
+//
+
+import SwiftUI
+
+struct TestViewKiki: View {
+    var body: some View {
+        Text("Hello, World! Azis, Bare")
+    }
+}
+
+#Preview {
+    TestViewKiki()
+}
