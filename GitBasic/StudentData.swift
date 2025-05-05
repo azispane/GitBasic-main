856a39db46d6c7aa16b859cdfd4a4635d7438ee3
Azis <mikhsanazisp@gmail.com>
2025-05-05 17:20:11 +0700

    add new struct

---
+++ GitBasic/StudentData.swift
@@ -0,0 +1,19 @@
+//
+//  StudentData.swift
+//  GitBasic
+//
+//  Created by M Ikhsan Azis Pane on 05/05/25.
+//
+
+import Foundation
+
+
+struct Student{
+    var id: Int
+    var name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
