6373f1da220c1bcf2c55823ba84a976892724943
Azis <mikhsanazisp@gmail.com>
2025-05-05 17:33:54 +0700

    add files

---
+++ GitBasic/File1.swift
@@ -0,0 +1,10 @@
+//
+//  File1.swift
+//  GitBasic
+//
+//  Created by M Ikhsan Azis Pane on 05/05/25.
+//
+
+import Foundation
+
+var status = true
---
+++ GitBasic/File2.swift
@@ -0,0 +1,11 @@
+//
+//  File2.swift
+//  GitBasic
+//
+//  Created by M Ikhsan Azis Pane on 05/05/25.
+//
+
+import Foundation
+
+
+var id = 1
